a92a43afeb4945aa344d2626032e9647b703cfa6
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-02-13 01:06:40 +0300

    remove: luck from stats

--- component/stats.py
+++ component/stats.py
@@ -4,7 +4,6 @@ class Armor(Stat): pass
 class MagicResist(Stat): pass
 class AttackDamage(Stat): pass
 class AttackSpeed(Stat): pass
-class Luck(Stat): pass
 
 class Health(Meter): pass
 class Mana(Meter): pass
