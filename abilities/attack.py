ab7bd5af1fe0b78e04f11c5777709ee152329f66
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-02-15 20:28:04 +0300

    fix: attack ability damage

--- abilities/attack.py
+++ abilities/attack.py
@@ -6,6 +6,6 @@ def attack_handler(world, attacker_id, target_id):
     if not damage:
         return False
     
-    world.damage_system.queue_damage(attacker_id, target_id, PhysicalDamageType(), damage.value)
+    world.damage_system.queue_damage(attacker_id, target_id, PhysicalDamageType(), damage.effective_value)
 
     return True
