9e5724464270c04b0c7d008608e80bf0f217c101
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-02-15 20:20:49 +0300

    feat: test scripts

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from world import World
 from system.clock import Clock
 from game import GameLoop
 
-world = World()
+from test_scrpts import test_script
 
-unit_id = world.entity_system.create_unit("flaneur")
-world.logger.log_unit(unit_id)
+world = World()
+test_script(world)
 
 clock = Clock()
 loop = GameLoop(world, clock)
---
+++ test_scrpts.py
@@ -0,0 +1,21 @@
+from component.target import Target
+
+def test_script(world):
+    flaneur = world.entity_system.create_unit("flaneur")
+    world.logger.log_unit(flaneur)
+
+    print()
+
+    meowmeow = world.entity_system.create_unit("meowmeow")
+    world.logger.log_unit(meowmeow)
+
+    print()
+
+    flaneur_attack = world.entity_system.create_autoattack(flaneur)
+
+    print()
+
+    world.add_component(flaneur, Target(meowmeow))
+    world.combat_system.cast(flaneur_attack)
+
+    world.logger.log_unit(meowmeow)
