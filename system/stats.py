3874228292ac2072e275cd4e4782c4ea1d3fc2ac
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-02-20 05:10:39 +0300

    feat: update stats method

--- system/stats.py
+++ system/stats.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 from component.stats import Stats
 from shared.statref import StatRef
 
@@ -8,67 +10,83 @@ class StatsSystem:
     def update_modifiers(self, entity_id, stat_type): # TODO
         self.world.get_component(entity_id, stat_type).modifiers = []
 
-    def _on_stat_update(self, entity_id, update_list: list[StatRef]):
+    def _update_stats(self, entity_id, stats: list[StatRef]):
+        update_list = set(stats)
+
+        # Circular dependency possible
+        max_iterations = 15 # TODO: get from config
+        iterations = 0
+
+        while update_list and iterations < max_iterations:
+            for stat_ref in update_list:
+                self._update_stat_value(entity_id, stat_ref)
+
+            update_list = self._get_children(entity_id, update_list)
+            iterations += 1
+
+    def _get_children(self, entity_id, parents: list[StatRef]) -> Set[StatRef]:
+        # Gets StatRef's dependent on list of parent StatRef's
         stats_component = self.world.get_component(entity_id, Stats)
 
         if stats_component is None:
-            self.world.logger.error(f"Entity {entity_id} has no {component_type.__name__}")
-            return
+            self.world.logger.error(f"Entity {entity_id} has no stats")
+            return set()
         
-        to_update_list: list[StatRef] = []
+        child_stats: Set[StatRef] = set()
 
         stats = stats_component.set
         for component_type in stats:
             stat_component = self.world.get_component(entity_id, component_type)
 
-            for updated_value in update_list:
-                to_update_list.append(*self._dependent_component_values(stat_component, updated_value))
+            if not hasattr(stat_component, 'formulas'):
+                continue
+
+            for parent in parents:
+                for child in self._get_component_children(stat_component, parent):
+                    child_stats.add(child)
         
-        # TODO: update all values to update
+        return child_stats
 
-        return update_list
-
-    def _dependent_component_values(self, component, stat_value_data: StatRef) -> list[StatRef]:
-        value_data_list: list[StatRef] = []
+    def _get_component_children(self, component, parent: StatRef) -> Set[StatRef]:
+        # Gets StatRef's of component's values dependent on parent StatRef
+        value_data_list: Set[StatRef] = set()
 
         for value_name in component.formulas:
             formula = component.formulas[value_name]
             requirements = formula.requires
 
-            if stat_value_data in requirements:
-                value_data_list.append(StatRef(type(component), value_name))
+            if parent in requirements:
+                value_data_list.add(StatRef(type(component), value_name))
         
         return value_data_list
 
-    def update_stat_value(self, entity_id, component_type, value_name):
-        component = self.world.get_component(entity_id, component_type)
+    def _update_stat_value(self, entity_id, stat_ref: StatRef):
+        component = self.world.get_component(entity_id, stat_ref.component_type)
 
         if component is None:
-            self.world.logger.error(f"Entity {entity_id} has no {component_type.__name__}")
+            self.world.logger.error(f"Entity {entity_id} has no {stat_ref.component_type.__name__}")
             return
 
-        formula = component.formulas[value_name]
+        formula = component.formulas[stat_ref.value_name]
 
         setattr(
             component, 
-            value_name, 
+            stat_ref.value_name, 
             self._calculate_formula(entity_id, formula)
         )
-
-        return component_type
 
     def _calculate_formula(self, unit_id, formula):
         kwargs = {}
 
         for require in formula.requires:
-            component_type, value_name = require
-
-            component = self.world.get_component(unit_id, component_type)
-            if component is None or not hasattr(component, value_name):
-                self.world.logger.error(f"{formula.__name__} requires {component_type.__name__}.{value_name}")
+            component = self.world.get_component(unit_id, require.component_type)
+            if component is None or not hasattr(component, require.value_name):
+                self.world.logger.error(
+                    f"{formula.__name__} requires {require.component_type.__name__}.{require.value_name}"
+                )
                 return 0
             
-            arg_name = f"{component_type.formula_key}_{value_name}"
-            kwargs[arg_name] = getattr(component, value_name)
+            arg_name = f"{require.component_type.formula_key}_{require.value_name}"
+            kwargs[arg_name] = getattr(component, require.value_name)
         
         return formula.calculate(**kwargs)
