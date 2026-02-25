01cd8953c4e8bc9be7ea65b2726e46cbeb9a1cc2
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-02-25 18:15:46 +0300

    feat: resource cost component

--- component/ability.py
+++ component/ability.py
@@ -1,16 +1,23 @@
+from dataclasses import dataclass
+from typing import Dict
+
 from component.meter import Meter
 
 class AbilityEffect:
     def __init__(self, handler = lambda world, caster, target: None):
         self.handler = handler
 
+@dataclass
 class Owner:
-    def __init__(self, unit_id):
-        self.unit_id = unit_id
+    unit_id: int = None
 
+@dataclass
 class CastTime:
-    def __init__(self, value = 0):
-        self.value = value
+    value: int = 0
+
+@dataclass
+class ResourceCost:
+    cost: Dict[type, int]
 
 class Cooldown(Meter): 
     # value = 1 - ability is ready
