ee3d607b3129d5f408dbf9a8618fd3796d20c467
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-03-04 00:09:13 +0300

    fix: resource restore behaviour

--- game/behaviour/abilities/resource_restore.py
+++ game/behaviour/abilities/resource_restore.py
@@ -9,22 +9,16 @@ class ResourceRestoreBehaviour(Behaviour):
     def __init__(self, resource: Dict[type, int]):
         self.resource = resource
 
-    def on_cast(self, world, ability_id):
-        from game.system.ability import AbilitySystem
-        ability_system = world.get_system(AbilitySystem)
-
-        cast_time = 0
-        cast_time_component = world.get_component(ability_id, CastTime)
-        if cast_time_component:
-            cast_time = cast_time_component.value
-
-        ability_system.schedule(CastEndCommand(ability_id), cast_time)
-
-    def on_cast_end(self, world, ability_id):
+    def _schedule_restore(self, world, ability_id):
         from game.system.ability import AbilitySystem
         ability_system = world.get_system(AbilitySystem)
 
         caster = world.get_component(ability_id, Owner)
         if caster and not caster.unit_id is None:
             ability_system.schedule(ResourceRestoreCommand(caster.unit_id, self.resource))
-            caster_id = caster.unit_id
+
+    def on_cast_end(self, world, ability_id):
+        self._schedule_restore(world, ability_id)
+
+    def on_attack(self, world, ability_id):
+        self._schedule_restore(world, ability_id)
