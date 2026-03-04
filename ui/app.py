fcdf8a3ccab9e93892969f498c467c5429e2e69f
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2026-03-04 06:20:05 +0300

    feat: log window

--- ui/app.py
+++ ui/app.py
@@ -1,17 +1,24 @@
 from textual.app import App, ComposeResult
+from textual.containers import Vertical
 from pathlib import Path
 
 from ui.component.combat import Combat
+from ui.component.log_window import LogWindow
 
 class GameApp(App):
     BINDINGS = [
         ("escape", "quit", "Quit"),
     ]
 
-    CSS_PATH = list(Path(__file__).parent.glob("style/*.css"))
+    CSS_PATH = list(Path(__file__).parent.glob("style/*.tcss"))
 
     def compose(self) -> ComposeResult:
-        yield Combat([
-            ("Team 1", [("Flaneur", 100), ("Rogue", 80)]),
-            ("Team 2", [("Guard", 120), ("Mage", 70)]),
-        ])
+        with Vertical():
+            yield Combat([
+                ("Team 1", [("flaneur", 100)]),
+                ("Team 2", [("meowmeow", 100)]),
+            ])
+            yield LogWindow()
+
+    def get_log(self) -> LogWindow:
+        return self.query_one(LogWindow)
---
+++ ui/component/log_window.py
@@ -0,0 +1,12 @@
+from textual.app import ComposeResult
+from textual.containers import Vertical
+from textual.widgets import Static, RichLog
+
+
+class LogWindow(Vertical):
+    def compose(self) -> ComposeResult:
+        yield Static("Combat Log", classes="log-title")
+        yield RichLog(highlight=True, markup=True, wrap=True, id="log")
+
+    def write(self, text):
+        self.query_one("#log", RichLog).write(text)
